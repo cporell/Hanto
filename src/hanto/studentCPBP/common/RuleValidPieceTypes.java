c469d489116bd3971595d80e44b1e4cc96e41557
bpeake13 <bennypeake@gmail.com>
2016-04-26 15:13:56 -0400

    Rules for valid piece types

---
+++ src/hanto/studentCPBP/common/RuleValidPieceTypes.java
@@ -0,0 +1,32 @@
+package hanto.studentCPBP.common;
+
+import java.util.HashSet;
+
+import hanto.common.HantoException;
+import hanto.common.HantoPieceType;
+
+public class RuleValidPieceTypes implements GenericHantoRuleCollection.IRule
+{
+	private HashSet<HantoPieceType> allowedTypes = new HashSet<>();
+	
+	public RuleValidPieceTypes(HantoPieceType[] allowedTypes)
+	{
+		for(HantoPieceType type : allowedTypes)
+		{
+			this.allowedTypes.add(type);
+		}
+	}
+	
+	@Override
+	public void checkBoard(IHantoBoard board) throws HantoException
+	{
+		for(CommonHantoPiece piece : board.getPieces())
+		{
+			if(!allowedTypes.contains(piece))
+			{
+				throw new HantoException("Not a valid piece type.");
+			}
+		}
+	}
+
+}
